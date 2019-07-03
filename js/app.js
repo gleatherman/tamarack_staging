daf296436752054aa17f8de06e7a6739d227dad5
Paul Carroll <paul@stublag.com>
2019-07-03 14:56:24 -0700

    Reworked site to match latest design spec.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 $( document ).ready(function() {
 
-    // Smart Scroll
+//     // Smart Scroll
 
-     var options = {
-        mode: "vp", // "vp", "set"
-        autoHash: false,
-        sectionScroll: true,
-        initialScroll: true,
-        keepHistory: false,
-        sectionWrapperSelector: ".section-wrapper",
-        sectionClass: "section",
-        animationSpeed: 300,
-        breakpoint: 800,
-        headerHash: "top",
-        breakpoint: null,
-        eventEmitter: null,
-        dynamicHeight: false
-    };
-  $.smartscroll(options);
+//      var options = {
+//         mode: "vp", // "vp", "set"
+//         autoHash: false,
+//         sectionScroll: true,
+//         initialScroll: true,
+//         keepHistory: false,
+//         sectionWrapperSelector: ".section-wrapper",
+//         sectionClass: "section",
+//         animationSpeed: 1000,
+//         breakpoint: 800,
+//         headerHash: "top",
+//         breakpoint: null,
+//         eventEmitter: null,
+//         dynamicHeight: false
+//     };
+//   $.smartscroll(options);
 
+
+});
+
+$(window).scroll(function() {
+    var theta = $(window).scrollTop() / 2500 % Math.PI;
+    $('.sunburst-green').css({ transform: 'rotate(-' + theta + 'rad)' });
+    $('.sunburst-gold').css({ transform: 'rotate(' + theta + 'rad)' });
 });
