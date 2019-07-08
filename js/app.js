bfd362fca9a2acdff2bff8f12447e3ae861b9de1
Paul Carroll <paul@stublag.com>
2019-07-08 16:24:18 -0700

    Added updating nav & smooth scrolling.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,52 @@
+
+
 $( document ).ready(function() {
+
+    // MenuSpy
+    var elm = document.querySelector('#main-nav');
+    var ms = new MenuSpy(elm, {
+    activeClass: 'active'
+    });
+
+    // Smooth Scrolling
+    // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+        ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top
+            }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+            } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+            };
+            });
+        }
+        }
+    });
+
 
 //     // Smart Scroll
 
@@ -19,11 +67,11 @@ $( document ).ready(function() {
 //     };
 //   $.smartscroll(options);
 
-
 });
 
 $(window).scroll(function() {
-    var theta = $(window).scrollTop() / 2500 % Math.PI;
+    var theta = $(window).scrollTop() / 2250 % Math.PI;
     $('.sunburst-green').css({ transform: 'rotate(-' + theta + 'rad)' });
     $('.sunburst-gold').css({ transform: 'rotate(' + theta + 'rad)' });
 });
+
