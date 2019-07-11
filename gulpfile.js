4c1cb733e85b3f0153b6c501ea1a107a5006a386
Paul Carroll <paul@stublag.com>
2019-07-11 14:09:28 -0700

    Set up Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+
+var gulp = require('gulp'),
+    exec = require('child_process').exec,           // Run child processes with exec
+    browserSync = require('browser-sync'),          // Live reload and more
+    sass = require('gulp-sass'),                    // Compile SASS (scss) files
+    // prefix = require('gulp-autoprefixer'),	        // CSS browser prefixing
+    cleanCSS = require('gulp-clean-css');			// Minify CSS files
+
+
+// DEFAULT TASK
+// This task runs when you type 'gulp' on the command line
+//--------------------------------------------
+gulp.task('default', function(){
+
+    // SCSS/CSS WATCHERS
+    gulp.watch('./scss/**/*.scss', gulp.series('scss'));
+});
+
+
+// SASS/CSS
+//--------------------------------------------
+gulp.task('scss', function () {
+    return gulp.src(['./scss/app.scss'])
+
+    // Compile SASS
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+
+    // Add browser specific css prefixes
+    // .pipe(prefix({ browsers: ['last 2 versions', 'Explorer >= 9'] }))
+
+    // Minify CSS
+    .pipe(cleanCSS({compatibility: 'ie9'}))
+
+    // Save minified CSS file to compiled site
+    .pipe(gulp.dest('./css/'))
+});
