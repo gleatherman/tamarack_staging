c9f64bbd9604dc522ecb613e0430f9be7d6bee62
Paul Carroll <paul@stublag.com>
2019-07-15 10:58:31 -0700

    Figured out scrolling behavior / cleaned things up.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-
-
 $( document ).ready(function() {
 
     // Founders
-
     $('.founder-card-small.michael-small').on('click', function(){
-        $('#founders').addClass('founders-active michael-active');
+        $('.founders').addClass('founders-active michael-active');
     });
 
     $('.founder-card-small.larry-small').on('click', function(){
-        $('#founders').addClass('founders-active larry-active');
+        $('.founders').addClass('founders-active larry-active');
     });
 
     $('.michael-large').on('click', function(){
-        $('#founders').addClass('michael-active').removeClass('larry-active');
+        $('.founders').addClass('michael-active').removeClass('larry-active');
     });
     $('.larry-large').on('click', function(){
-        $('#founders').addClass('larry-active').removeClass('michael-active');
+        $('.founders').addClass('larry-active').removeClass('michael-active');
     });
 
     $('.close-x').on('click', function(){
-        $('#founders').removeClass('founders-active');
+        $('.founders').removeClass('founders-active');
         setTimeout(function(){
-            $('#founders').removeClass('michael-active larry-active');
+            $('.founders').removeClass('michael-active larry-active');
         }, 300);
         return false;
-
     });
 
 
@@ -37,83 +33,57 @@ $( document ).ready(function() {
     });
 
 
-    // MenuSpy
-    var elm = document.querySelector('#main-nav');
-    var ms = new MenuSpy(elm, {
-    activeClass: 'active'
-    });
+    // Scrollify
+    $.scrollify({
+        section : ".section",
+        sectionName : "section-name",
+        interstitialSection : "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1200,
+        offset : 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: false,
+        touchScroll:true,
+        before:function(i,panels) {
 
-    // Smooth Scrolling
-    // https://css-tricks.com/snippets/jquery/smooth-scrolling/
-    // Select all links with hashes
-    $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-        // On-page links
-        if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-        ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-            scrollTop: target.offset().top
-            }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-            } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-            };
-            });
-        }
-        }
+            // Update active nav items
+            var ref = panels[i].attr("data-section-name");
+            $("#main-nav .active").removeClass("active");
+            $("#main-nav").find("a[href=\"#" + ref + "\"]").addClass("active");
+
+        },
+        after:function() {
+        },
+        afterResize:function() {},
+        afterRender:function() {}
     });
 
 
-//     // Smart Scroll
-
-//      var options = {
-//         mode: "vp", // "vp", "set"
-//         autoHash: false,
-//         sectionScroll: true,
-//         initialScroll: true,
-//         keepHistory: false,
-//         sectionWrapperSelector: ".section-wrapper",
-//         sectionClass: "section",
-//         animationSpeed: 1000,
-//         breakpoint: 800,
-//         headerHash: "top",
-//         breakpoint: null,
-//         eventEmitter: null,
-//         dynamicHeight: false
-//     };
-//   $.smartscroll(options);
+    // Smooth scroll when clicking nav items / logo
+    $("#main-nav a, #logo").on("click",function() {
+        $.scrollify.move($(this).attr("href"));
+        return false;
+    });
 
 });
 
+// Scroll-based effects
 $(window).scroll(function() {
+
+    // Spin sunbursts on scroll
     var theta = $(window).scrollTop() / 2250 % Math.PI;
     $('.sunburst-green').css({ transform: 'rotate(-' + theta + 'rad)' });
     $('.sunburst-gold').css({ transform: 'rotate(' + theta + 'rad)' });
 
     // Collapse nav when not at the top
-    if ( $(window).scrollTop() > 0 ) {
+    if ( $(window).scrollTop() > 150 ) {
         $("nav").addClass('collapsed');
     } else {
         $("nav").removeClass('collapsed');
     }
+
 });
 
